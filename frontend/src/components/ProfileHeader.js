21c47478a5397c49da246350c80d00cfb3ab85e6
yuri <yurialec@hotmail.com>
2022-12-28 15:30:07 -0300

    Create the Profile.js page

--- frontend/src/components/ProfileHeader.js
+++ frontend/src/components/ProfileHeader.js
@@ -1,7 +1,43 @@
-export default function ProfileHeader(){
-    return(
-        <div>
-           ProfileHeader 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import UserProfile from '../images/UserProfile.jpg'
+
+export default function ProfileHeader() {
+    return (
+        <div className="grid grid-cols-3 mb-10">
+            <div className="bg-green p-3 rounded flex items-start justify-center">
+                <img
+                    className="rounded-full"
+                    alt='Profile'
+                    src={UserProfile}
+                    width="150"
+                />
+            </div>
+            <div className='p-3 text-gray-600 cols-span-2'>
+                <div className='flex items-center'>
+                    <h1 className='inline-block text-3xl align-bottom block'>
+                        User369852
+                    </h1>
+                    <button className='bg-white ml-3 text-gray-800 font-semibold py-1 px-2 border border-gray-400 rounded text-sm'>Edit Profile</button>
+                    <a className='ml-3' href=''>
+                        <FontAwesomeIcon icon="gear" className='text-2xl leading-6' />
+                    </a>
+                </div>
+                <div className="flex-row py-5 max-w-sm hidden lg:flex">
+                    <div className="basis-1/2 ">
+                        <strong>230</strong> posts
+                    </div>
+                    <div className="basis-1/2">
+                        <strong className="mr-1">1050</strong> followers
+                    </div>
+                    <div className="basis-1/2">
+                        <strong className="mr-1">30</strong>following
+                    </div>
+                </div>
+                <h3 className='font-bold'>Yuri Alec | Web Dev</h3>
+                <p>velit sed ullamcorper morbi tincidunt</p>
+                <p>turpis in eu mi bibendum</p>
+                <a href='https://www.linkedin.com/in/yuri-alec-3976b227/' target="_blank" className='text-blue-900 font-bold'>My Linkedin</a>
+            </div>
         </div>
     );
 }
