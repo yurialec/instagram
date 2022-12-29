5e05dec17236b4ff27de0fb5d99425470bf77f1e
yuri <yurialec@hotmail.com>
2022-12-29 19:59:04 -0300

    Install Headless UI and create Dropdown Menu

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,6 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Logo from '../images/logo.png'
 import UserProfile from '../images/UserProfile.jpg'
 import { Link } from "react-router-dom";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+
+export const classNames = (...classes) =>{
+    return classes.filter(Boolean).join(" ");
+}
 
 export default function Navbar() {
 
@@ -69,13 +75,106 @@ export default function Navbar() {
                             </li>
                         </ul>
                     </div>
-                    <Link to="/yuri_alec">
-                        <img
-                            className="rounded-full w-25 w-full"
-                            src={UserProfile}
-                            alt="User Profile"
-                        />
-                    </Link>
+                    <Menu
+                        as="div"
+                        className="relative inline-block text-left"
+                    >
+                        <div>
+                            <Menu.Button className="inline-block w-8 h-8 justify-center bg-white text-sm font-medium text-gray-700">
+                                <img
+                                    className="rounded-full"
+                                    src={UserProfile}
+                                    width="24"
+                                />
+                            </Menu.Button>
+                        </div>
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                        >
+                            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none divide-y divide-gray-100">
+                                <div className="py-1">
+                                    <Menu.Item>
+                                        {({active }) => (
+                                            <Link
+                                                to={"/yuri_alec"}
+                                                className={`${
+                                                    classNames(active
+                                                    ? 'bg-gray-100 text-gray-900'
+                                                    : 'text-gray-700'
+                                                    , 'block px-4 py-2 text-sm text-gray-700')
+                                                    }`}
+                                            >
+                                                <FontAwesomeIcon
+                                                    icon="fa-solid fa-user"
+                                                    className="mr-3"
+                                                />
+                                                Profile
+                                            </Link>
+                                        )}
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        {({active }) => (
+                                            <Link
+                                                to={"/yuri_alec"}
+                                                className={`${
+                                                    classNames(active
+                                                    ? 'bg-gray-100 text-gray-900'
+                                                    : 'text-gray-700'
+                                                    , 'block px-4 py-2 text-sm text-gray-700')
+                                                    }`}
+                                            >
+                                                <FontAwesomeIcon
+                                                    icon="fa-solid fa-bookmark"
+                                                    className="mr-3"
+                                                />
+                                                Saved
+                                            </Link>
+                                        )}
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        {({active }) => (
+                                            <Link
+                                                to={"/yuri_alec"}
+                                                className={`${
+                                                    classNames(active
+                                                    ? 'bg-gray-100 text-gray-900'
+                                                    : 'text-gray-700'
+                                                    , 'block px-4 py-2 text-sm text-gray-700')
+                                                    }`}
+                                            >
+                                                <FontAwesomeIcon
+                                                    icon="fa-solid fa-gear"
+                                                    className="mr-3"
+                                                />
+                                                Settings
+                                            </Link>
+                                        )}
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        {({active }) => (
+                                            <Link
+                                                to={"/yuri_alec"}
+                                                className={`${
+                                                    classNames(active
+                                                    ? 'bg-gray-100 text-gray-900'
+                                                    : 'text-gray-700'
+                                                    , 'block px-4 py-2 text-sm text-gray-700')
+                                                    }`}
+                                            >
+                                                Logout
+                                            </Link>
+                                        )}
+                                    </Menu.Item>
+                                </div>
+                            </Menu.Items>
+                        </Transition>
+                    </Menu>
                 </div>
             </div>
         </nav>
